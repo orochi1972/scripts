f5c905b2ee972c68023b52af9424b941ae03e17c
Zihan <orochi1972@163.com>
2021-07-17 14:35:40 +0800

    Add files via upload

--- ahr999.py
+++ ahr999.py
@@ -11,9 +11,10 @@ from email.mime.text import MIMEText
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
-
-sender = 'Nathan971209@gmail.com'
+from pyvirtualdisplay import Display
+sender = 'Orochi1972@126.com'
 receivers = ['orochi1972@163.com']
 def send_email(body_of_email,subject):
     body_of_email = body_of_email
@@ -24,15 +25,22 @@ def send_email(body_of_email,subject):
     msg['From'] = sender
     msg['To'] = ','.join(receivers)
 
-    s = smtplib.SMTP_SSL(host = 'smtp.gmail.com', port = 465)
-    s.login(user = sender, password = 'zzh7758521')
+    s = smtplib.SMTP_SSL(host = 'smtp.126.com', port = 465)
+    s.login(user = sender, password = 'TFNSPLCBLDARZYGO')
     s.sendmail(sender, receivers, msg.as_string())
     s.quit()
 def get_indicator():
-    browser = webdriver.Chrome()
+    display = Display(visible=0,size=(1366,768))
+    display.start()
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(options=chrome_options)
     browser.get("https://ahr999.com/")
     strhtml = browser.page_source
-    browser.close()
+    browser.quit()
+    display.stop()
     try:
         soup=BeautifulSoup(strhtml,'lxml')
         data =str(soup.find_all('tspan')[0])
@@ -43,25 +51,24 @@ def get_indicator():
 def sleeptime(hour,mins,sec):
     return hour*3600 + mins*60 + sec;
 
-# if __name__ == '__main__':
-#     seconds = sleeptime(6,0,0);
-#     count_hours= 0
-#     while True:
-#         if count_hours >= 12*2*7:
-#             title= 'weekly ahr999 indicator notification'
-#             body = 'current ahr999 is '+str(get_indicator())
-#             send_email(body,title)
-#             count_hours=0
-#         elif get_indicator()<=0.45:
-#             title = 'Time to all in ETH!!!!!!!!'
-#             body = "finally you've got the chance"
-#         time.sleep(seconds)
-#         count_hours+=6
 if __name__ == '__main__':
-    title= ' ahr999 test'
-    body = 'current ahr999 is '+str(get_indicator())
-    send_email(body,title)
+    seconds = sleeptime(6,0,0);
+    count_hours= 0
+    while True:
+        if count_hours >= 12*2*7:
+            title= 'weekly ahr999 indicator notification'
+            body = 'current ahr999 is '+str(get_indicator())
+            send_email(body,title)
+            count_hours=0
+        elif get_indicator()<=0.45:
+            title = 'Time to all in ETH!!!!!!!!'
+        time.sleep(seconds)
+        count_hours+=6
+#if __name__ == '__main__':
+#    title= ' ahr999 test'
+#    body = 'current ahr999 is '+str(get_indicator())
+#    send_email(body,title)
 
+                                                             57,1          76%
 
-
-
+                                                              28,1          43%
